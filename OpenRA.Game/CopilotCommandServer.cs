fbac060a710d2ecf23964405423ae9706e352440
anantheparty <anantheparty0@gmail.com>
2024-07-15 01:16:43 +0800

    修复camera/move写错了

--- OpenRA.Game/CopilotCommandServer.cs
+++ OpenRA.Game/CopilotCommandServer.cs
@@ -125,7 +125,7 @@ namespace OpenRA
 							case "/api/produce":
 								result = OnStartProdunctionCommand?.Invoke(json, world);
 								break;
-							case " /api/camera/follow":
+							case " /api/camera/move":
 								result = OnCameraMoveCommand?.Invoke(json, world);
 								break;
 							case "/api/units/select":
