7f05a410f55674a07a18e73883b378f37888e83e
anantheparty <anantheparty0@gmail.com>
2025-07-16 23:18:44 +0800

    加一个启动脚本，方便启动

---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 默认参数值
+mod="Game.Mod=copilot"
+loadsave="Game.LoadSave=None"
+port="Game.CopilotPort=7445"
+debug="Game.CopilotDebug=True"
+
+# 标记参数是否已提供
+has_mod=false
+has_loadsave=false
+has_port=false
+has_debug=false
+
+# 其他参数收集
+other_args=()
+
+# 遍历传入参数
+for arg in "$@"; do
+  case "$arg" in
+    Game.Mod=*) mod="$arg"; has_mod=true ;;
+    Game.LoadSave=*) loadsave="$arg"; has_loadsave=true ;;
+    Game.CopilotPort=*) port="$arg"; has_port=true ;;
+    Game.CopilotDebug=*) debug="$arg"; has_debug=true ;;
+    *) other_args+=("$arg") ;;
+  esac
+done
+
+# 组装命令行
+cmd="./launch-game.sh $mod $loadsave $port $debug"
+for arg in "${other_args[@]}"; do
+  cmd="$cmd $arg"
+done
+
+# 输出并执行
+echo "$cmd"
+eval "$cmd"
